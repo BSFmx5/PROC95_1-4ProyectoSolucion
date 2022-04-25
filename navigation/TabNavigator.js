fbfc6ef4a93bf14f710f580c8db7c7fce7e238f9
BFSmx5 <97541881+BSFmx5@users.noreply.github.com>
2022-04-25 09:18:33 -0500

    Update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -27,8 +27,8 @@ const BottomTabNavigator = () => {
                 inactiveTintColor: 'gray',
             }}
         >
-            <Tab.Screen name="Feed" component={Feed} />
-            <Tab.Screen name="CreatePost" component={CreatePost} />
+            <Tab.Screen name="Alimentar" component={Feed} />
+            <Tab.Screen name="Crear Post" component={CreatePost} />
         </Tab.Navigator>
     );
 }
